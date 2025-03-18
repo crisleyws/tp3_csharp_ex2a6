bead29b16b7972471f85446f2b3a86bb1f4de625
crisleyws <crisley.simoes@al.infnet.edu.br>
2025-03-17 21:51:44 -0300

    first

--- obj/Debug/net8.0/tp3_csharp_ex2a6.AssemblyInfo.cs
+++ obj/Debug/net8.0/tp3_csharp_ex2a6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp3_csharp_ex2a6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de564a77eb956b63d8d677ef88d74c06e39df46")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp3_csharp_ex2a6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp3_csharp_ex2a6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
