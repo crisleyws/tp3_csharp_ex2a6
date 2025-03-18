6de564a77eb956b63d8d677ef88d74c06e39df46
crisleyws <crisley.simoes@al.infnet.edu.br>
2025-03-17 21:51:30 -0300

    Initial commit

---
+++ Ingresso.cs
@@ -0,0 +1,67 @@
+﻿namespace tp3_csharp_ex2a6
+{
+    public class Ingresso
+    {
+        public string NomeDoShow;
+        public double Preco;
+        public int QuantidadeDisponivel;
+
+        public string GetNomeDoShow()
+        {
+            return NomeDoShow;
+        }
+
+        public void SetNomeDoShow(string novoNome)
+        {
+            NomeDoShow = novoNome;
+        }
+
+        public double GetPreco()
+        {
+            return Preco;
+        }
+
+        public void SetPreco(double novoPreco)
+        {
+            Preco = novoPreco;
+        }
+
+
+        public int GetQuantidadeDisponivel()
+        {
+            return QuantidadeDisponivel;
+        }
+
+        public void SetQuantidadeDisponivel(int novaQuantidade)
+        {
+            QuantidadeDisponivel = novaQuantidade;
+        }
+
+
+
+
+        public void AtualizarPreco(double novoPreco)
+        {
+            Preco = novoPreco;
+        }
+
+        public void AtualizarQuantidade(int novaQuantidade)
+        {
+            QuantidadeDisponivel = novaQuantidade;
+        }
+
+        public void ExibirIngresso()
+        {
+            Console.WriteLine("Nome do show: " + NomeDoShow);
+            Console.WriteLine("Preço: " + Preco);
+            Console.WriteLine("Quantidade disponível: " + QuantidadeDisponivel);
+        }
+
+        public void ExibirIngressoGet()
+        {
+            Console.WriteLine($"Nome do show: {GetNomeDoShow}");
+            Console.WriteLine($"Nome do show: {GetPreco}");
+            Console.WriteLine($"Nome do show: {GetQuantidadeDisponivel}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using tp3_csharp_ex2a6;
+
+public class Program
+{
+    public static void Main()
+    {
+        Ingresso ingresso = new Ingresso();
+        ingresso.NomeDoShow = "Show do C#";
+        ingresso.Preco = (150.50);
+        ingresso.QuantidadeDisponivel = (800);
+        ingresso.ExibirIngresso();
+
+        Console.WriteLine();
+        Console.WriteLine("atualizado");
+        Console.WriteLine();
+
+        ingresso.AtualizarPreco(100);
+        ingresso.AtualizarQuantidade(1000);
+        ingresso.ExibirIngresso();
+
+        Console.WriteLine();
+        Console.WriteLine("Usando get e set");
+        Console.WriteLine();
+
+        Ingresso ingresso2 = new Ingresso();
+        ingresso2.SetNomeDoShow("Show do C# com get e set");
+        ingresso2.SetPreco(150.50);
+        ingresso2.SetQuantidadeDisponivel(800);
+        ingresso2.ExibirIngresso();
+
+        Console.WriteLine();
+        Console.WriteLine("atualizado com get e set");
+        Console.WriteLine();
+
+        ingresso2.SetPreco(180.0);
+        ingresso2.SetQuantidadeDisponivel(650);
+        ingresso2.ExibirIngressoGet();
+    }
+}
